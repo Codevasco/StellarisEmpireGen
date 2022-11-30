92fe27a77e17d44ddb6d687a198800623784b2de
Vasco Gonçalves <vascofg99alt@gmail.com>
2022-11-30 15:39:32 +0000

    Logic changes

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -4,14 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("");
-        System.out.println("---------------");
-        System.out.println("");
-        
-        System.out.println("Stellaris Empire Generator v2");
-
         Empire empire = new Empire();
+        
 
-        System.out.println("---------------");
     }
 }
